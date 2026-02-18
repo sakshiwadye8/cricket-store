2686ca2b0388b39928822cc1194c05d1f822d77b
sakshiwadye8 <sakshiwadye@gmail.com>
2026-02-18 05:33:01 +0530

    Update constants.php

--- config/constants.php
+++ config/constants.php
@@ -1,19 +1,18 @@
+
 <?php
 
-// Start Session (only if not started)
-if (session_status() == PHP_SESSION_NONE) {
-    session_start();
-}
-
-// Create Constants (only if not defined)
+// Database Constants
 define('LOCALHOST', 'gateway01.ap-southeast-1.prod.aws.tidbcloud.com');
 define('DB_USERNAME', '3RnCJM8nYe7PLXW.root');
 define('DB_PASSWORD', 'fYv5wyj3Lzxfsfbq');
 define('DB_NAME', 'test'); // or your db name
 
-// Database Connection
+// Create connection
 $conn = mysqli_connect(LOCALHOST, DB_USERNAME, DB_PASSWORD, DB_NAME, 4000);
- or die(mysqli_error());
-$db_select = mysqli_select_db($conn, DB_NAME) or die(mysqli_error());
+
+// Check connection
+if(!$conn){
+    die("Connection Failed: " . mysqli_connect_error());
+}
 
 ?>
