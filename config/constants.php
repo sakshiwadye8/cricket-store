2463f9d73a415f4cf25eec1f832cde16db80a355
sakshiwadye8 <sakshiwadye@gmail.com>
2026-02-18 05:44:53 +0530

    Update constants.php

--- config/constants.php
+++ config/constants.php
@@ -1,11 +1,13 @@
 <?php
 
+// Site URL
+define('SITEURL', 'https://cricket-store-2.onrender.com/');
+
 // Database Constants
 define('LOCALHOST', 'gateway01.ap-southeast-1.prod.aws.tidbcloud.com');
 define('DB_USERNAME', '3RnCJM8nYe7PLXW.root');
 define('DB_PASSWORD', 'fYv5wyj3Lzxfsfbq');
 define('DB_NAME', 'test'); // or your db name
-
 // Create connection with SSL
 $conn = mysqli_init();
 
@@ -22,9 +24,12 @@ mysqli_real_connect(
     MYSQLI_CLIENT_SSL
 );
 
-// Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
+}
+
+?>
+
 }
 
 ?>
