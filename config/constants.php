03ded92898d859b0df3245514b907a2de8c43d0a
sakshiwadye8 <sakshiwadye@gmail.com>
2026-02-18 05:39:03 +0530

    Update constants.php

--- config/constants.php
+++ config/constants.php
@@ -1,4 +1,3 @@
-
 <?php
 
 // Database Constants
@@ -7,12 +6,26 @@ define('DB_USERNAME', '3RnCJM8nYe7PLXW.root');
 define('DB_PASSWORD', 'fYv5wyj3Lzxfsfbq');
 define('DB_NAME', 'test'); // or your db name
 
-// Create connection
-$conn = mysqli_connect(LOCALHOST, DB_USERNAME, DB_PASSWORD, DB_NAME, 4000);
+// Create connection with SSL
+$conn = mysqli_init();
+
+mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);
+
+mysqli_real_connect(
+    $conn,
+    LOCALHOST,
+    DB_USERNAME,
+    DB_PASSWORD,
+    DB_NAME,
+    4000,
+    NULL,
+    MYSQLI_CLIENT_SSL
+);
 
 // Check connection
-if(!$conn){
-    die("Connection Failed: " . mysqli_connect_error());
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
 }
 
 ?>
+
