dac7043539481f5836a349806ff8b8d1b3be61a1
sakshiwadye8 <sakshiwadye@gmail.com>
2026-02-20 00:09:35 +0530

    Update constants.php

--- config/constants.php
+++ config/constants.php
@@ -3,40 +3,28 @@
 // Site URL
 define('SITEURL', 'https://cricket-store-2.onrender.com/');
 
-// Database Constants
-$conn = mysqli_connect(
-    "gateway01.ap-southeast-1.prod.aws.tidbcloud.com",
-    "oi4z2DV1NMQGcKv.root",
-    "huknkaUfLA8NavGB",
-    "test",
-    4000
-);
-);
-;
+// Database connection for TiDB Cloud
 
-// Create connection with SSL
 $conn = mysqli_init();
 
+// Enable SSL (recommended for TiDB)
 mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);
 
+// Connect to TiDB
 mysqli_real_connect(
     $conn,
-    LOCALHOST,
-    DB_USERNAME,
-    DB_PASSWORD,
-    DB_NAME,
-    4000,
+    "gateway01.ap-southeast-1.prod.aws.tidbcloud.com", // Host
+    "2DszSUbPUgJhoMQ.root",                            // Username
+    "7I6XwHVtSfFHFkYu",                                // Password
+    "cricket_store",                                   // Database
+    4000,                                              // Port
     NULL,
     MYSQLI_CLIENT_SSL
 );
 
+// Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
 ?>
-
-}
-
-?>
-
